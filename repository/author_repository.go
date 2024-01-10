c74fe0e0ae234b34bcc1c614d1f2839d41f9a253
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-10 11:25:26 +0700

    create interface for author repo

--- repository/author_repository.go
+++ repository/author_repository.go
@@ -1,1 +1,9 @@
 package repository
+
+import "todo-clean-arch/model"
+
+type AuthorRepository interface {
+	GetByEmail(email string) (model.Author, error)
+	Get(id string) (model.Author, error)
+	List() ([]model.Author, error)
+}
