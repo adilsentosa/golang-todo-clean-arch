9189f8231fc499f3c4b994227a955b6116e38209
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-11 09:54:01 +0700

    create package delivery to store controller and server

---
+++ delivery/server.go
@@ -0,0 +1,42 @@
+package delivery
+
+import (
+	"fmt"
+	"todo-clean-arch/config"
+	"todo-clean-arch/repository"
+	"todo-clean-arch/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	authorUC usecase.AuthorUsecase
+	taskUC   usecase.TaskUsecase
+	engine   *gin.Engine
+	host     string
+}
+
+func (s *Server) Run() {
+	if err := s.engine.Run(s.host); err != nil {
+		panic(fmt.Errorf("failed to start server %v", err))
+	}
+}
+
+func NewServer() *Server {
+	db := config.ConnectDB()
+
+	taskRepository := repository.NewTaskRepository(db)
+	authorRepository := repository.NewAuthorRepository(db)
+	authorUseCase := usecase.NewAuthorUseCase(authorRepository)
+	taskUseCase := usecase.NewTaskUseCase(taskRepository, authorUseCase)
+
+	engine := gin.Default()
+	host := fmt.Sprintf(":%s", "8080")
+
+	return &Server{
+		authorUC: authorUseCase,
+		taskUC:   taskUseCase,
+		engine:   engine,
+		host:     host,
+	}
+}
