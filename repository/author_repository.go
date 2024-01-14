983096d89f371e0fec10300c7a0cb7439510fa88
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-14 13:39:50 +0700

    chore: get json handler

--- repository/author_repository.go
+++ repository/author_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"time"
 	"todo-clean-arch/config"
@@ -11,6 +12,7 @@ import (
 type AuthorRepository interface {
 	GetByEmail(email string) (model.Author, error)
 	Get(id string) (model.Author, error)
+	GetJson(id string) (model.Author, error)
 	List(id string) ([]model.Author, error)
 	Update(author model.Author) (model.Author, error)
 	Delete(id string) error
@@ -61,46 +63,46 @@ func (a *authorRepository) Get(id string) (model.Author, error) {
 	return author, nil
 }
 
-// func (a *authorRepository) Get(id string) (model.Author, error) {
-// 	var author model.Author
-// 	// query := "SELECT id,name,email,created_at,updated_at FROM authors WHERE id = $1"
-// 	query := `SELECT
-// 	  a.id,
-// 	  a.name,
-// 	  a.email,
-//     a.updated_at,
-// 	  a.created_at,
-// 	  array_agg(
-// 		  jsonb_build_object(
-// 			  'id', t.id,
-// 			  'title', t.title,
-// 			  'content', t.content,
-// 			  'author_ID', t.author_id,
-// 			  'created_at', t.created_at
-// 		  )
-// 	  ) AS tasks
-//   FROM authors a
-//   JOIN tasks t  ON a.id = t.author_id
-//   WHERE a.id = $1
-//   GROUP BY a.id,a.name, a.email,a.created_at,a.updated_at`
-// 	var tasks []model.Task
-// 	// var tasks []map[string]interface{}
-// 	var stringTasks string
-//
-// 	err := a.db.QueryRow(query, id).Scan(&author.ID, &author.Name, &author.Email, &author.CreatedAt, &author.UpdatedAt, &stringTasks)
-// 	if err != nil {
-// 		return model.Author{}, err
-// 	}
-// 	err = json.Unmarshal([]byte(stringTasks), &tasks)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	fmt.Printf("%v", stringTasks)
-// 	fmt.Println()
-// 	fmt.Println(tasks)
-//
-// 	return author, nil
-// }
+func (a *authorRepository) GetJson(id string) (model.Author, error) {
+	var author model.Author
+	// query := "SELECT id,name,email,created_at,updated_at FROM authors WHERE id = $1"
+	query := `SELECT
+	  a.id,
+	  a.name,
+	  a.email,
+    a.updated_at,
+	  a.created_at,
+	  array_agg(
+		  jsonb_build_object(
+			  'id', t.id,
+			  'title', t.title,
+			  'content', t.content,
+			  'author_ID', t.author_id,
+			  'created_at', t.created_at
+		  )
+	  ) AS tasks
+  FROM authors a
+  JOIN tasks t  ON a.id = t.author_id
+  WHERE a.id = $1
+  GROUP BY a.id,a.name, a.email,a.created_at,a.updated_at`
+	var tasks []model.Task
+	// var tasks []map[string]interface{}
+	var stringTasks string
+
+	err := a.db.QueryRow(query, id).Scan(&author.ID, &author.Name, &author.Email, &author.CreatedAt, &author.UpdatedAt, &stringTasks)
+	if err != nil {
+		return model.Author{}, err
+	}
+	err = json.Unmarshal([]byte(stringTasks), &tasks)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("%v", stringTasks)
+	fmt.Println()
+	fmt.Println(tasks)
+
+	return author, nil
+}
 
 func (a *authorRepository) GetByEmail(email string) (model.Author, error) {
 	var author model.Author
